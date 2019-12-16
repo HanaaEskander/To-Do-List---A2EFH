973ddd2b52047d007f36870774a06a55bd4c7f3f
AimanAlmureish <aiman.almureish@gmail.com>
2019-12-16 15:10:25 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import TodoHeader from "./TodoHeader";
-// import TodoList from "./TodoList";
+import TodoList from "./TodoList";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <TodoHeader />
-        {/* <TodoList /> */}
+        <TodoList />
       </header>
     </div>
   );
